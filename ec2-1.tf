d96866f20378ba75131e5ba2696fe2cee865ca48
isambituma <isambituma@gmail.com>
2023-11-04 09:22:51 -0500

    week10

--- ec2-1.tf
+++ ec2-1.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ec2-1" {
     ami = "ami-01eccbf80522b562b"
     vpc_security_group_ids = [aws_security_group.ec2.id]
     subnet_id = aws_subnet.private_subnet1.id
+    key_name = "week10"
 user_data = file("userdata.sh")
     instance_type = "t2.micro"
     tags = {
@@ -12,6 +13,7 @@ resource "aws_instance" "ec2-2" {
     ami = "ami-01eccbf80522b562b"
     vpc_security_group_ids = [aws_security_group.ec2.id]
     subnet_id = aws_subnet.private_subnet2.id
+    key_name = "week10"
     user_data = file("userdata.sh")
   instance_type = "t2.micro"
   tags = {
